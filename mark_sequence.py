500d992032b7bc794b57e64ec410932b2bb63c0d
Damien Picard <dam.pic@free.fr>
2020-08-06 11:37:49 +0200

    Get fields from actual loaded template

--- mark_sequence.py
+++ mark_sequence.py
@@ -23,6 +23,7 @@ import getpass
 import datetime
 import argparse
 import os
+import json
 from tempfile import TemporaryDirectory
 import fileseq
 
@@ -31,7 +32,7 @@ default_template = {
     # "settings": {
     #     ...
     # },
-    "output_fields": [
+    "fields": [
         {
             "field": "shot_name",
             "direction": "NorthWest",
@@ -102,9 +103,14 @@ def mark_image(path, output_path, data):
 
     # Add annotations for each field to the list of directions
     # This has the effect of concatenating various fields for a given direction
-    for field in data['template']['output_fields']:
+    for field in data['template']['fields']:
         direction = field['direction']
-        value = field['string'] % data[field['field']]
+        value = field['string']
+        # Try formatting the string with the value from the command line
+        try:
+            value %= data[field['field']]
+        except TypeError:
+            pass
         if not direction in directions:
             directions[direction] = ''
         directions[direction] += (value)
@@ -123,9 +129,13 @@ def mark_image(path, output_path, data):
     args.append('%s' % output_path)  # Output path
     proc = subprocess.run(args, check=True)
 
+# TODO find better way to parse args multiple times
+class NonExitingArgumentParser(argparse.ArgumentParser):
+    def exit(self, status=0, message=None):
+        pass
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
+    parser = NonExitingArgumentParser(
         description='Make an annotated movie output from a list of images')
     parser.add_argument('sequence', type=str,
                         help='file sequence, typically a frame in the sequence')
@@ -138,30 +148,43 @@ if __name__ == "__main__":
     parser.add_argument('--offset', type=int, default=0,
                         help='offset for renaming frames')
 
-    parser.add_argument('--comment', type=str, default="",
-                        help='comment added underneath the video')
-    parser.add_argument('--shot', type=str,
-                        help='shot name in top left corner of the video')
-    parser.add_argument('--user', type=str, default=getpass.getuser(),
-                        help='user responsible for the render')
-    parser.add_argument('--hostname', type=str, default="",
-                        help='render run from this hostname')
-    parser.add_argument('--source', type=str, default="",
-                        help='file used to generate this sequence')
-    parser.add_argument('--date', type=str, default=datetime.datetime.now().strftime("%d-%m-%y %H:%M"),
-                        help='override date')
-    parser.add_argument('--copyright', metavar='date', type=str, default="(C) Ne pas diffuser",
-                        help='copyright information')
     args = parser.parse_args()
 
+    # Load in template from supplied file. If none given, use default one.
+    if args.template is None:
+        print(None)
+        template = default_template
+    else:
+        with open(os.path.abspath(args.template), 'r') as f:
+            template = json.load(f)
+
+    for field in template['fields']:
+        parser.add_argument('--' + field['field'], type=str, default='')
+
+    args = parser.parse_args()
+
+    # Special fields: for each special field, give a default if it is
+    # specified in the template but not overriden on command line
+    for field in template['fields']:
+        if field['field'] == 'date' and not args.date:
+            args.date = datetime.datetime.now().strftime("%d-%m-%y %H:%M")
+        if field['field'] == 'user' and not args.user:
+            args.user = getpass.getuser()
+        if field['field'] == 'hostname' and not args.hostname:
+            import platform
+            args.hostname = platform.node()
+
+    # Create temporary directory for images
+    # TODO fix temp dir
     if args.mark_dir:
         mark_dir = args.mark_dir
         os.makedirs(mark_dir, exist_ok=True)
     else:
         mark_dir = TemporaryDirectory().name
 
-    data = {'font_size': 16,}
+    data = {'font_size': 16, }
     data.update(vars(args))
+    data['template'] = template
 
     resolution_x = None
     resolution_y = None
@@ -169,14 +192,9 @@ if __name__ == "__main__":
     file_sequence = fileseq.findSequenceOnDisk(os.path.abspath(args.sequence))
     frame_set = file_sequence.frameSet()
 
-    data['total_images'] = len(frame_set)
-
-    # Load in template from supplied file. If none given, use default one.
-    if args.template is None:
-        data['template'] = default_template
-    else:
-        with open(args.template, 'r') as f:
-            data['template'] = json.load(f)
+    for field in template['fields']:
+        if field['field'] == 'total_images' and not args.total_images:
+            data['total_images'] = len(frame_set)
 
     # for i in range(args.start_frame, args.end_frame + 1):
     for i, image_number in enumerate(file_sequence.frameSet()):
@@ -192,6 +210,11 @@ if __name__ == "__main__":
             resolution_y = res_y
             data['resolution_y'] = int(res_y)
 
-        data['normalized_frame_number'] = i - args.offset + 1
-        data['frame_number'] = image_number
+        # Special fields: for each special field, give a default if it is
+        # specified in the template but not overriden on command line
+        for field in template['fields']:
+            if field['field'] == 'frame_number' and not args.frame_number:
+                data['frame_number'] = image_number
+            if field['field'] == 'normalized_frame_number' and not args.normalized_frame_number:
+                data['normalized_frame_number'] = i - args.offset + 1
         mark_image(image_source, image_marked, data)
