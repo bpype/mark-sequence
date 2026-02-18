fc7c781810e8ca6daab33fe5b9e14f19565899a3
Damien Picard <damien@les-fees-speciales.coop>
2026-02-18 09:43:50 +0100

    Revert "Try using h.264 codec once again"
    Turns out the reason for mjpeg was to get better interactive
    scrubbing. Good reason.

--- mark_sequence.py
+++ mark_sequence.py
@@ -344,7 +344,8 @@ class SequenceMarker:
         ffmpeg_args.extend(["-vf", video_filter])
 
         # Video codec
-        ffmpeg_args.extend(["-c:v", "h264", "-crf", "21", "-preset", "slow", "-pix_fmt", "yuv420p", "-movflags", "+faststart"])
+        ffmpeg_args.extend(["-c:v", "mjpeg", "-q:v", "3"])
+        # ffmpeg_args.extend(["-c:v", "h264", "-crf", "21", "-preset", "slow", "-pix_fmt", "yuv420p", "-movflags", "+faststart"])
 
         os.makedirs(os.path.dirname(destination), exist_ok=True)
         ffmpeg_args.extend(["%s" % (destination)])
